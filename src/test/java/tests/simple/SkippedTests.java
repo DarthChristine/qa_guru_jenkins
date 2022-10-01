0a5a51d7810ded59ed5088cb77907040e0688ab5
DarthChistine <kpodolnova@gmail.com>
2022-10-01 18:20:16 +0400

    second commit - tests

--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -1,4 +1,40 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SkippedTests {
+
+    @Test
+    @Disabled("Some reason")
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test4() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test5() {
+        assertTrue(true);
+    }
+
 }
